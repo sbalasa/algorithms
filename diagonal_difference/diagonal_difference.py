c3e06d4a18752b7d4eeab51b4db32f266d2681cd
Santhosh Balasa <santhosh.kbr@gmail.com>
2019-07-16 11:09:48 +0530

    Removing unnecessary comments

--- diagonal_difference/diagonal_difference.py
+++ diagonal_difference/diagonal_difference.py
@@ -6,14 +6,8 @@ import random
 import re
 import sys
 
-#
-# Complete the 'diagonalDifference' function below.
-#
-# The function is expected to return an INTEGER.
+
 # The function accepts 2D_INTEGER_ARRAY arr as parameter.
-#
-
-
 def diagonalDifference(arr):
     new_arr = [j for i in arr for j in i]
     ld = 0
